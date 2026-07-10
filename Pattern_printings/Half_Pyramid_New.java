c7e11dd8086d1751ef4ab850d37b7d28bb593cec
Sarthak Gaikwad <sarthakgaikwad756@gmail.com>
2026-07-11 02:01:29 +0530

    chage_name

---
+++ Pattern_printings/Half_Pyramid_New.java
@@ -0,0 +1,51 @@
+// Half Pyramids
+
+// *    
+// **   
+// ***  
+// **** 
+// *****
+// *****
+// **** 
+// ***  
+// **   
+// *
+
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Rows: \n");
+        int rows = sc.nextInt();
+        int nst=1;
+        int nsp=rows-1;
+        
+        //   System.out.println();  // New Line 
+        
+        for(int i=1;i<=2*rows;i++){
+            // each row
+            
+            // print star
+            for(int j=1;j<=nst;j++){
+                System.out.print("*");
+            }
+            
+            // print space
+            for(int j=1;j<=nsp;j++){
+                System.out.print(" ");
+            }
+            
+            if(i<rows){
+                nst++;
+                nsp--;
+            }
+            if(i>rows){
+                nst--;
+                nsp++;
+            }
+            System.out.println();// New Line 
+        }
+       
+    }
+}
